bc12695185b13babdeac747c77c6391ec2ae4892
TopVirusMinus <mwmma5000@gmail.com>
2023-12-31 15:53:55 +0200

    Feature: Added Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import "./App.css";
+import { useState } from "react";
 import ProductCard from "./components/ProductCard";
 import { productList } from "./data";
+import Modal from "./components/ui/Modal";
+import Button from "./components/ui/Button";
 
-function App() {
+const App = () => {
+  let [isOpen, setIsOpen] = useState(true);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
   const productCardsList = productList.map((product) => (
     <ProductCard product={product} key={product.id} />
   ));
 
   return (
     <main className="container mx-auto">
+      <div className="mt-2">
+        <Button onClick={openModal} className="bg-indigo-600 text-white">
+          Add new Product
+        </Button>
+      </div>
+      <Modal
+        title="ADD A NEW PRODUCT"
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-4">
         {productCardsList}
       </div>
     </main>
   );
-}
+};
 
 export default App;
